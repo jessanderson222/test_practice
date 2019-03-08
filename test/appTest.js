ff0ecfc6cf3c84eb99af857ac50a80fba04ebbe2
jessanderson222 <jessanderson222@gmail.com>
2019-03-08 14:51:42 -0500

    added string test

--- test/appTest.js
+++ test/appTest.js
@@ -3,8 +3,13 @@ const sayHello = require("../app").sayHello;
 // const app = require("../app");
 
 describe("App", function() {
-  it("app should return hello", function() {
+  it("sayHello should return hello", function() {
     let result = sayHello();
     assert.equal(result, "hello");
+  });
+
+  it("sayHello should return type string", function() {
+    let result = sayHello();
+    assert.typeOf(result, "string");
   });
 });
