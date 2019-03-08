c55f74dd848ee5eb444aca3d32dbab0500ee8956
jessanderson222 <jessanderson222@gmail.com>
2019-03-08 15:00:16 -0500

    nested describes to separate addNumbers and sayHello functions

--- test/appTest.js
+++ test/appTest.js
@@ -1,15 +1,34 @@
 const assert = require("chai").assert;
-const sayHello = require("../app").sayHello;
-// const app = require("../app");
+// const sayHello = require("../app").sayHello;
+// const addNumbers = require("../app").addNumbers;
+const app = require("../app");
+
+//Results
+sayHelloResult = app.sayHello();
+addNumberResult = app.addNumbers(5, 5);
 
 describe("App", function() {
-  it("sayHello should return hello", function() {
-    let result = sayHello();
-    assert.equal(result, "hello");
+  describe("sayHello", function() {
+    it("sayHello should return hello", function() {
+      // let result = app.sayHello();
+      assert.equal(sayHelloResult, "hello");
+    });
+
+    it("sayHello should return type string", function() {
+      // let result = app.sayHello();
+      assert.typeOf(sayHelloResult, "string");
+    });
   });
 
-  it("sayHello should return type string", function() {
-    let result = sayHello();
-    assert.typeOf(result, "string");
+  describe("addNumbers", function() {
+    it("addNumbers should be above 5", function() {
+      // let result = app.addNumbers(5, 5);
+      assert.isAbove(addNumberResult, 5);
+    });
+
+    it("addNumbers should return type number", function() {
+      // let result = app.addNumbers(5, 5);
+      assert.typeOf(addNumberResult, "number");
+    });
   });
 });
